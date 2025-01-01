f7404b74f4aa00adbb082dd91445d4598dd51187
365Bit <20995752+365Bit@users.noreply.github.com>
2025-01-01 23:06:11 +0100

    Improves layout of navigation bar

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,24 +1,35 @@
 <template>
-    <v-card>
-        <v-toolbar color="primary" extended>
-            <v-toolbar-title>DeMAF</v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-file-input label="File" variant="outlined"></v-file-input>
-            <v-spacer></v-spacer>
-            <v-select label="Technology" :items="['Helm', 'Kubernetes']" variant="outlined"></v-select>
-            <v-spacer></v-spacer>
-            <v-select clearable label="Options" :items="['flat', 'partial']" multiple variant="outlined"></v-select>
-            <v-spacer></v-spacer>
-            <v-text-field label="Commands" variant="outlined"></v-text-field>
+    <v-app>
+        <v-app-bar color="primary" prominent>
+            <v-app-bar-title>DeMAF</v-app-bar-title>
+            <v-row></v-row>
+            <v-row>
+                <v-file-input label="File" min-width="200px" variant="outlined"></v-file-input>
+                <v-spacer></v-spacer>
+                <v-select label="Technology" :items="['Helm', 'Kubernetes']" min-width="150px"
+                    variant="outlined"></v-select>
+                <v-spacer></v-spacer>
+                <v-select clearable label="Options" :items="['flat', 'partial']" min-width="150px" multiple
+                    variant="outlined"></v-select>
+                <v-spacer></v-spacer>
+                <v-text-field label="Commands" min-width="200px" variant="outlined"></v-text-field>
+            </v-row>
             <v-spacer></v-spacer>
             <v-btn rounded="LG">Transform</v-btn>
             <v-spacer></v-spacer>
             <template v-slot:extension>
-                <v-tabs>
+                <v-tabs align-with-title>
                     <v-tab>Start</v-tab>
                 </v-tabs>
             </template>
-        </v-toolbar>
-        <v-tabs-window></v-tabs-window>
-    </v-card>
+        </v-app-bar>
+        <v-main>
+            <v-tabs-window>
+                <v-tabs-window-item>
+                    <v-card>
+                    </v-card>
+                </v-tabs-window-item>
+            </v-tabs-window>
+        </v-main>
+    </v-app>
 </template>
