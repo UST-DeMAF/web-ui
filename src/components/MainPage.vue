68c38a1c9162140c550545b45c20676a649f0087
365Bit <20995752+365Bit@users.noreply.github.com>
2025-01-03 23:07:09 +0100

    Adds icons

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -18,33 +18,46 @@
             <v-btn rounded="LG">Transform</v-btn>
             <v-spacer></v-spacer>
             <template v-slot:extension>
-                <v-tabs align-with-title>
-                    <v-tab>Start</v-tab>
+                <v-tabs align-with-title v-model="tab">
+                    <v-tab value="start">Start</v-tab>
                 </v-tabs>
             </template>
         </v-app-bar>
         <v-main>
-            <v-tabs-window>
-                <v-card>
+            <v-tabs-window v-model="tab">
+                <v-tabs-window-item value="start">
                     <v-row>
-                        <v-col cols="2">
-                            <v-card>
-                                <v-card-title>Last transformations:</v-card-title>
-                                <v-card-text>
-                                    Test
-                                </v-card-text>
-                            </v-card>
+                        <v-col align-self="center" cols="2">
+                            <v-container>
+                                <v-card>
+                                    <v-card-title>Last transformations:</v-card-title>
+                                    <v-card-text>
+                                        Test
+                                    </v-card-text>
+                                </v-card>
+                            </v-container>
                         </v-col>
-                        <v-col>
-                            <v-card>
-                                <v-card-text>
-                                    Test
-                                </v-card-text>
-                            </v-card>
+                        <v-col align-self="center" cols="8">
+                            <v-container>
+                                <v-row>
+                                    <v-icon icon="fas fa-cloud-arrow-up" size="64px" />
+                                </v-row>
+                                <v-row>
+                                    To start drag and drop or upload a file.
+                                </v-row>
+                            </v-container>
                         </v-col>
                     </v-row>
-                </v-card>
+                </v-tabs-window-item>
             </v-tabs-window>
         </v-main>
     </v-app>
 </template>
+
+<script>
+  export default {
+    data: () => ({
+      tab: null,
+    }),
+  }
+</script>
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -5,14 +5,23 @@
  */
 
 // Styles
+import '@fortawesome/fontawesome-free/css/all.css'
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 
 // Composables
+import { aliases, fa } from 'vuetify/iconsets/fa'
 import { createVuetify } from 'vuetify'
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
+  icons: {
+    defaultSet: 'fa',
+    aliases,
+    sets: {
+      fa,
+    },
+  },
   theme: {
     defaultTheme: 'dark',
   },
