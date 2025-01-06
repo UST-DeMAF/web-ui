85cf7ef18a755e5de3df0171492672aca42c3614
Marcel W <st163937@stud.uni-stuttgart.de>
2025-01-06 20:14:06 +0100

    ADD transformation call at analysismanager container; NOT FULLY WORKING
    Terraform container throws errors

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -6,7 +6,7 @@
             <v-row>
                 <v-file-input v-model="uploadedFile" label="File" min-width="200px" variant="outlined"></v-file-input>
                 <v-spacer></v-spacer>
-                <v-select v-model="selectedTechnology" label="Technology" :items="['Helm', 'Kubernetes']" min-width="150px"
+                <v-select v-model="selectedTechnology" label="Technology" :items="['Helm', 'Kubernetes', 'Terraform']" min-width="150px"
                     variant="outlined"></v-select>
                 <v-spacer></v-spacer>
                 <v-select v-model="selectedOptions" clearable label="Options" :items="['flat', 'partial']" min-width="150px" multiple
@@ -102,6 +102,8 @@
         const formData = new FormData();
         formData.append('file', this.uploadedFile);
 
+
+        //BUG: fails if transformation is called a second time
         try {
           const response = await fetch('http://localhost:3000/upload', {
             method: 'POST',
@@ -113,8 +115,38 @@
           }
 
           console.log('File uploaded successfully');
+
+          // Call the transform endpoint after the file is uploaded
+          await this.callAnalysisManagerTransformation();
         } catch (error) {
           console.error('Error uploading file:', error);
+        }
+      },
+      async callAnalysisManagerTransformation() {
+        const tsdm = {
+          technology: this.selectedTechnology.toLowerCase(), // Convert to lowercase
+          locationURL: 'file://./usr/share/' + this.uploadedFile.name, // Use file:// protocol
+          commands: this.commands ? this.commands.split(',').map(cmd => cmd.trim()) : [''], // Ensure commands is an array of strings
+          options: this.selectedOptions,
+        };
+
+        try {
+          const response = await fetch('http://localhost:8080/demaf/transform', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(tsdm),
+          });
+
+          if (!response.ok) {
+            throw new Error('Failed to start transformation process');
+          }
+
+          const data = await response.json();
+          console.log('Transformation process started with ID:', data);
+        } catch (error) {
+          console.error('Error starting transformation process:', error);
         }
       }
     },
